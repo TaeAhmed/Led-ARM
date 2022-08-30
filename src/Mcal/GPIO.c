bbad57425d79f0d7b93a2ce914e54227e6f7c5c4
TaeAhmed <45271134+TaeAhmed@users.noreply.github.com>
2022-08-30 21:36:57 +0200

    debug 2 added Unlock to DIO driver,still no output

--- src/Mcal/GPIO.c
+++ src/Mcal/GPIO.c
@@ -1,5 +1,6 @@
 #include "Common/Platform_Types.h"
 #include "Common/Mcu_Hw.h"
+#include "Common/Bit_Math.h"
 #include "GPIO.h"
 
 #ifndef Port_Macro
@@ -11,18 +12,27 @@
 #define GPIO_PORT_E 4
 #define GPIO_PORT_F 5
 #endif
-
+/*Local vars*/
+static boolean state =0;
+/*--*/
+/*Local function prototype*/
 volatile static uint32_t* GPIO_GetPort_Data(uint8_t port);
-void GPIO_Channel_Dir(volatile uint32_t *Dport, uint8_t pin, boolean direction);
-void GPIO_ClkEnable(uint8_t port);
-void GPIO_Channel_Den(volatile uint32_t *Dport, uint8_t pin);
-
+static  void GPIO_Channel_Dir(volatile uint32_t *Dport, uint8_t pin, boolean direction);
+static void GPIO_ClkEnable(uint8_t port);
+static void GPIO_Channel_Den(volatile uint32_t *Dport, uint8_t pin);
+static void GPIO_Unlock(volatile uint32_t *Dport);
+/*--*/
 /*API functions*/
 void GPIO_WriteChannel(uint8_t port, uint8_t pin, boolean state){
 *GPIO_GetPort_Data(port) |= (state<<pin);
 }
 void GPIO_ToggleChannel(uint8_t port, uint8_t pin){
-*GPIO_GetPort_Data(port) ^= (1<<pin);
+  if(!state) {
+	SET_BIT_PER_BB(*GPIO_GetPort_Data(port),pin);
+	state =!state;}
+	else {
+	CLEAR_BIT_PER_BB(*GPIO_GetPort_Data(port),pin);
+	state=!state;}
 }
 boolean GPIO_ReadChannel(uint8_t port, uint8_t pin){
 boolean state = 0;
@@ -37,26 +47,32 @@ switch(port){
 	case GPIO_PORT_A:
 		GPIO_Channel_Den(&PORTA_DEN,pin);
 		GPIO_Channel_Dir(&PORTA_DIR,pin,1);
+		GPIO_Unlock(&PORTA_LOCK);
 		break;
 	case GPIO_PORT_B: 
 		GPIO_Channel_Den(&PORTB_DEN,pin);
 		GPIO_Channel_Dir(&PORTB_DIR,pin,1);
+		GPIO_Unlock(&PORTB_LOCK);
 		break;
 	case GPIO_PORT_C:
 		GPIO_Channel_Den(&PORTC_DEN,pin);
 		GPIO_Channel_Dir(&PORTC_DIR,pin,1);
+		GPIO_Unlock(&PORTC_LOCK);
 		break;
 	case GPIO_PORT_D:
 		GPIO_Channel_Den(&PORTD_DEN,pin);
 		GPIO_Channel_Dir(&PORTD_DIR,pin,1);
+		GPIO_Unlock(&PORTD_LOCK);
 		break;
 	case GPIO_PORT_E:
 		GPIO_Channel_Den(&PORTE_DEN,pin);
 		GPIO_Channel_Dir(&PORTE_DIR,pin,1);	
+		GPIO_Unlock(&PORTE_LOCK);
 		break;
 	case GPIO_PORT_F:
 		GPIO_Channel_Den(&PORTF_DEN,pin);
 		GPIO_Channel_Dir(&PORTF_DIR,pin,1);
+		GPIO_Unlock(&PORTF_LOCK);
 		break;
 }
 }
@@ -89,14 +105,17 @@ switch(port){
 }
 return DATA;
 }
-
-void GPIO_ClkEnable(uint8_t port){
+/*Local functions*/
+static void GPIO_ClkEnable(uint8_t port){
 SYSCTL_RCGCGPIO |= (1<<port);
 }
-void GPIO_Channel_Den(volatile uint32_t *Dport, uint8_t pin){
+static void GPIO_Channel_Den(volatile uint32_t *Dport, uint8_t pin){
   *Dport |= (1<<pin);
 }	
-void GPIO_Channel_Dir(volatile uint32_t *Dport, uint8_t pin, boolean direction){
+static void GPIO_Channel_Dir(volatile uint32_t *Dport, uint8_t pin, boolean direction){
 	*Dport |= (direction << pin);
+}
+static void GPIO_Unlock(volatile uint32_t *Dport){
+	*Dport = 0x4C4F434B;
 }
 /*--*/
